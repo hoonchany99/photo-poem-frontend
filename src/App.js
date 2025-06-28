b309e3337cea93ff39401bb5b13a76a63a6324b6
윤훈찬 <hoonchan@yunhunchan-ui-MacBookPro.local>
2025-06-28 18:51:14 +0900

    ga4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactGA from 'react-ga4';
 import { AnimatePresence } from 'framer-motion';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import UploadPage from './UploadPage';
@@ -7,6 +8,9 @@ import AdminPoemForm from './PoemDB';
 
 function App() {
   const location = useLocation();
+
+  ReactGA.initialize('G-Z71Q92KRNB'); // Replace with your actual Google Analytics 4 measurement ID
+  ReactGA.send('pageview');
 
   return (
     <AnimatePresence mode="wait">
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { ThemeProvider } from './context/ThemeContext';
 import './index.css';
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize('G-Z71Q92KRNB'); // 실제 GA4 ID로 바꿔야 함
+ReactGA.send('pageview'); // 초기 페이지뷰
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
